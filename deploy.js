3fa9ef70b01a6b2b8ff69a8ed0590b3ee28ff21b
Oscar Alfredo Leiva Salomón <oleiva@grupomerelec.com>
2022-11-21 08:45:24 -0600

    First commit

---
+++ deploy.js
@@ -0,0 +1,26 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { abi, evm } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'draft write fan congress autumn timber dignity insane width because chief luggage',
+  // remember to change this to your own phrase!
+  'https://rinkeby.infura.io/v3/ff59756300994d9da272fc83194838f8'
+  // remember to change this to your own endpoint!
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  console.log(JSON.stringify(abi));
+  provider.engine.stop();
+};
+deploy();
